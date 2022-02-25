c6462e91a81119e9bb497caf81c02c04a70ac5c0
gcamargocamposrocha <gcamargocamposrocha@guidewire.com>
2022-02-25 16:39:00 +0000

    Create transaction class

---
+++ wallet/transaction.js
@@ -0,0 +1,62 @@
+const uuid = require("uuid/v1");
+const { verifySignature } = require("../util");
+
+class Transaction {
+  constructor({ senderWallet, recipient, amount }) {
+    this.id = uuid();
+    this.outputMap = this.createOutputMap({ senderWallet, recipient, amount });
+    this.input = this.createInput({ senderWallet, outputMap: this.outputMap });
+  }
+
+  // Keeps track of the amount sent and the remaining balance on the sender wallet
+  createOutputMap({ senderWallet, recipient, amount }) {
+    const outputMap = {};
+
+    outputMap[recipient] = amount;
+    outputMap[senderWallet.publicKey] = senderWallet.balance - amount;
+
+    return outputMap;
+  }
+
+  // Creates an official transaction signed by the sender
+  createInput({ senderWallet, outputMap }) {
+    return {
+      timestamp: Date.now(),
+      amount: senderWallet.balance,
+      address: senderWallet.publicKey,
+      signature: senderWallet.sign(outputMap),
+    };
+  }
+
+  static validTransaction(transaction) {
+    const {
+      id,
+      input: { address, amount, signature },
+      outputMap,
+    } = transaction;
+
+    const outputTotal = Object.values(outputMap).reduce(
+      (total, outputAmount) => total + outputAmount
+    );
+
+    if (amount !== outputTotal) {
+      console.error(`Invalid transaction ${id} from ${address}`);
+      return false;
+    }
+
+    if (
+      !verifySignature({
+        publicKey: address,
+        data: outputMap,
+        signature: signature,
+      })
+    ) {
+      console.error(`Invalid signature from ${address}`);
+      return false;
+    }
+
+    return true;
+  }
+}
+
+module.exports = Transaction;
