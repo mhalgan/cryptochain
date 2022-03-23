b54c6027f5612dbf433308f704a3f8b428a53673
gcamargocamposrocha <gcamargocamposrocha@guidewire.com>
2022-03-23 16:37:06 +0000

    Add transaction update and fix edge cases

--- wallet/index.js
+++ wallet/index.js
@@ -1,5 +1,6 @@
 const { STARTING_BALANCE } = require("../config");
 const { ec, cryptoHash } = require("../util");
+const Transaction = require("./transaction");
 
 class Wallet {
   constructor() {
@@ -15,6 +16,14 @@ class Wallet {
   sign(data) {
     // Generates a signature based on some data and the wallet instance key pair
     return this.keyPair.sign(cryptoHash(data));
+  }
+
+  createTransaction({ amount, recipient }) {
+    if (amount > this.balance) {
+      throw new Error("Amount exceeds balance");
+    }
+
+    return new Transaction({ senderWallet: this, recipient, amount });
   }
 }
 
